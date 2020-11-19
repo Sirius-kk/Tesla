ef13861784eb541b334a2af4b0a290ee994c0530
“Sirius-kk <copander@163.com>
2020-11-19 20:58:35 +0800

    11

--- tesla/src/store/index.js
+++ tesla/src/store/index.js
@@ -8,13 +8,26 @@ import router from '../router'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    
+  // 共享状态(共享数据)
+  state: {/* 可以在任何一个组件中被访问到 */
+    // nav: [
+    //   {
+    //     activity: 
+    //   }
+    // ]
   },
+  // 专门来修改state中的共享状态的操作方法
+  //           使用方法
+  // this.$store.commit('方法名称'[,payload])
   mutations: {
+
   },
+  // 可以发送发送异步请求
+  // Actions提交的是Mutations，而不能直接变更state
   actions: {
+
   },
   modules: {
+
   }
 })
